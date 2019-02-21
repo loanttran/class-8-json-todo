4324e389c02d3385e5394c28856df38f64fefeb7
Loan Tran <loantranvn810@gmail.com>
2019-02-21 14:15:40 -0800

    Add in-class solutions

--- todos/tasks.py
+++ todos/tasks.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from .exceptions import (
-    InvalidTaskStatus, TaskAlreadyDoneException, TaskDoesntExistException)
+    InvalidTaskStatus, TaskAlreadyDoneException, InvalidTaskDueDateException, TaskDoesntExistException)
 from .utils import parse_date, parse_int
 
 
@@ -23,6 +23,9 @@ def create_task(tasks, name, description=None, due_on=None):
 
 
 def list_tasks(tasks, status='all'):
+    if status not in ['all', 'done', 'pending']:
+        raise InvalidTaskStatus()
+        
     task_list = []
     for idx, task in enumerate(tasks, 1):
         if task['due_on'] is not None:
@@ -39,12 +42,20 @@ def list_tasks(tasks, status='all'):
 
 def complete_task(tasks, name):
     new_tasks = []
+    found_flag = False 
+    id_name = parse_int(name)
 
-    for task in tasks:
-        if name == task['task']:
+    for task_id, task in enumerate(tasks, 1):
+        if name == task['task'] or task_id == id_name:
+            if task['status'] == 'done':
+                raise TaskAlreadyDoneException()
+            found_flag = True
             task = task.copy()
             task['status'] = 'done'
         new_tasks.append(task)
-
+    
+    if found_flag == False:
+        raise TaskDoesntExistException() 
+    
     return new_tasks
 
